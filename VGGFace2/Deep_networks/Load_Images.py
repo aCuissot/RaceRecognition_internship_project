32f6cb32f59b460ce89d775d21eca4cf624d7cba
aCuissot <harrypotter9752@gmail.com>
2019-05-21 14:12:27 +0200

    too complicated, should make labels when we generate the set

--- VGGFace2/Deep_networks/Load_Images.py
+++ VGGFace2/Deep_networks/Load_Images.py
@@ -1,10 +1,13 @@
 import os
 
 import keras
+from keras.utils import Sequence, multi_gpu_model
 from keras.preprocessing import image
 import numpy as np
 import cv2 as cv
 import pandas as pd
+from keras_applications.vgg16 import VGG16
+from keras_applications.xception import Xception
 
 csvBBGroundTruthTrainIds = pd.read_csv("../Data/bb_landmark/loose_bb_train.csv").loc[:, "NAME_ID"]
 csvBBGroundTruthTrain = pd.read_csv("../Data/bb_landmark/loose_bb_train.csv").set_index("NAME_ID")
@@ -88,5 +91,120 @@ def loadImages(model, trainBool=True):
             newImage = preprocessing(model, f + "/" + imageName.split(".")[0], image_path)
             X_train.append(newImage)  # bad idea
 
+
+from skimage.io import imread
+from skimage.transform import resize
+
+
+class My_Generator(Sequence):
+
+    def __init__(self, image_filenames, labels, batch_size):
+        self.image_filenames, self.labels = image_filenames, labels
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return np.ceil(len(self.image_filenames) / float(self.batch_size))
+
+    def __getitem__(self, idx):
+        batch_x = self.image_filenames[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.labels[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        return np.array([
+            resize(imread(file_name), (200, 200))
+            for file_name in batch_x]), np.array(batch_y)
+
+
+batch_size = 64
+num_epochs = 20
+num_training_samples = 1868
+num_validation_samples = 76
+
+
+def getIdsFromFile(file, FullPath):
+    idsList = []
+    if (FullPath != ""):
+        for line in file:
+            idsList.append(FullPath + "\\" + line)
+    else:
+        for line in file:
+            idsList.append(line)
+    return idsList
+
+
+trainSetIds = open('../Data/labels/homogeneousTrainSetIds.txt', "r")
+testSetIds = open('../Data/labels/homogeneousTestSetIds.txt', "r")
+training_filenames = getIdsFromFile(trainSetIds, "C:\\Users\\Cuissot\\PycharmProjects\\Data\\VGGFacesV2\\train")
+
+validation_filenames = getIdsFromFile(testSetIds, "C:\\Users\\Cuissot\\PycharmProjects\\Data\\VGGFacesV2\\train")
+
+
+def getId(list):
+    sublist = []
+    n = len(list)
+    for i in range(0, n, 2):
+        sublist.append(list[i])
+    return sublist
+
+
+def getEthnicity(list):
+    sublist = []
+    n = len(list)
+    for i in range(1, n, 2):
+        sublist.append(list[i])
+    return sublist
+
+
+def parseXML(xmlStr):
+    xmlStr = xmlStr.replace("<xml>\n", "")
+    xmlStr = xmlStr.replace("</xml>", "")
+    xmlStr = xmlStr.replace("<subject>\n", "")
+    xmlStr = xmlStr.replace("</subject>\n", "")
+
+    xmlStr = xmlStr.replace("<id>", "")
+    xmlStr = xmlStr.replace("<ethnicity>", "")
+    xmlStr = xmlStr.replace("</id>", "")
+    xmlStr = xmlStr.replace("</ethnicity>", "")
+
+    list = xmlStr.split("\n")
+    id = getId(list)
+    ethnicity = getEthnicity(list)
+    return id, ethnicity
+
+
+XMLtrain = open("../Data/labels/TrainXML.xml", "r")
+XMLtest = open("../Data/labels/TestXML.xml", "r")
+
+trainContent = XMLtrain.read()
+idData, ethnicityData = parseXML(trainContent)
+testContent = XMLtest.read()
+idDataTest, ethnicityDataTest = parseXML(testContent)
+idLabelListTrain = [idData, ethnicityData]
+idLabelListTest = [idDataTest, ethnicityDataTest]
+
+def mkGT(idList2LabelList, idLabelList):
+    labels = []
+    for i in idList2LabelList:
+        labels.append(idLabelList[1][idLabelList[0].index(i)])
+    return labels
+
+
+
+GT_training = mkGT(getIdsFromFile(trainSetIds, ""), idLabelListTrain)
+GT_validation = mkGT(getIdsFromFile(testSetIds, ""), idLabelListTest)
+
+my_training_batch_generator = My_Generator(training_filenames, GT_training, batch_size)
+my_validation_batch_generator = My_Generator(validation_filenames, GT_validation, batch_size)
+
+model = VGG16(weights=None)
+
+model.fit_generator(generator=my_training_batch_generator,
+                    steps_per_epoch=(num_training_samples // batch_size),
+                    epochs=num_epochs,
+                    verbose=1,
+                    validation_data=my_validation_batch_generator,
+                    validation_steps=(num_validation_samples // batch_size),
+                    use_multiprocessing=True,
+                    workers=16,
+                    max_queue_size=32)
 
 loadImages('')
