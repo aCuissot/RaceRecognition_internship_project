c530da1f432954bb5e1be410c1715fb08af54c9a
aCuissot <harrypotter9752@gmail.com>
2019-07-09 09:18:44 +0200

    evaluation of mobilenet, resnet

---
+++ VGGFace2/test_antonio/compute_confusion_matrix_resnet.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+
+import sys
+import os
+from glob import glob
+import numpy as np
+import sklearn
+
+import tensorflow as tf
+import keras
+
+print(keras.__version__)
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+keras.backend.set_session(session)
+
+from keras.engine import Model
+from keras.layers import Flatten, Dense, Dropout, Input, Reshape, Convolution2D, MaxPooling2D
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import EarlyStopping
+from keras.utils.vis_utils import plot_model
+from keras import backend as K
+
+from VGGFace2.test_antonio.dataset_tools import load_for_training, load_for_test, dataset_size, NUM_CLASSES
+
+batch_size = 64
+epochs = 64
+
+# learning rate schedule
+initial_learning_rate = 0.005
+learning_rate_decay_factor = 0.5
+learning_rate_decay_epochs = 6
+weight_decay = 5e-5
+MULTIPLIER_FOR_OLD_LAYERS = 0.1
+
+siz = 96
+
+dirnm = "exp-inp%d" % (siz)
+shape = (1, siz, siz, 3)
+
+print("Setting up for %s." % dirnm)
+
+# Load the basic network
+# model = keras.applications.mobilenet.MobileNet(input_shape=(224,224,3))
+from keras.applications.resnet50 import ResNet50
+source_model = ResNet50(input_shape=(shape[1], shape[2], shape[3]), include_top=False, classes=4)
+original_layers = [x.name for x in source_model.layers]
+x = source_model.get_layer('res5c_branch2c').output  # Ultimo livello della rete originale, senza dropout
+
+# Modify the network
+# x = keras.layers.GlobalAveragePooling2D()(last_layer)
+# x = keras.layers.Reshape((1, 1, 1024), name='reshape_1')(x)
+x = keras.layers.Dropout(0.5, name='dropout')(x)
+x = keras.layers.Flatten()(x)
+outS = x
+outS = Dense(NUM_CLASSES, activation="softmax", name='outS')(outS)
+res_model = Model(source_model.input, outS)  # Modelo solo gender
+res_model_multitask = res_model
+res_model_multitask.summary()
+# plot_model(res_model_multitask, to_file=os.path.join(dirnm, 'ResNet50.png'), show_shapes=True)
+
+
+# based on the learning rate multipliers
+for layer in res_model_multitask.layers:
+    layer.trainable = True
+learning_rate_multipliers = {}
+for layer_name in original_layers:
+    learning_rate_multipliers[layer_name] = MULTIPLIER_FOR_OLD_LAYERS
+# added levels will have lr_multiplier = 1
+new_layers = [x.name for x in source_model.layers if x.name not in original_layers]
+for layer_name in new_layers:
+    learning_rate_multipliers[layer_name] = 1
+
+# Prepare optimization with the lr_decay
+from VGGFace2.test_antonio.training_tools import Adam_lr_mult
+from VGGFace2.test_antonio.training_tools import step_decay_schedule
+
+adam_with_lr_multipliers = Adam_lr_mult(lr=initial_learning_rate, decay=weight_decay,
+                                        multipliers=learning_rate_multipliers)
+res_model_multitask.compile(adam_with_lr_multipliers,
+                            loss=['categorical_crossentropy'], metrics=['accuracy'])
+
+# Preparing callback
+if not os.path.isdir(dirnm):
+    os.mkdir(dirnm)
+filepath = os.path.join(dirnm, "resnet.{epoch:02d}-{val_loss:.2f}.hdf5")
+logdir = os.path.join(dirnm, 'tb_logs')
+lr_sched = step_decay_schedule(initial_lr=initial_learning_rate, decay_factor=learning_rate_decay_factor,
+                               step_size=learning_rate_decay_epochs)
+checkpoint = ModelCheckpoint(filepath, verbose=1, save_best_only=False)
+tbCallBack = keras.callbacks.TensorBoard(log_dir=logdir, write_graph=True, write_images=True)
+callbacks_list = [lr_sched, checkpoint, tbCallBack]
+
+# Evaluate
+
+if __name__ == '__main__':
+
+    print("Evaluation for %s is starting..." % dirnm)
+
+    # Load the dataset
+    val_dataset = './test1'
+
+    ckpntlist = ['resnet.20-1.46.hdf5']
+    # ckpntlist = [sys.argv[1]]
+    res_model_multitask.load_weights(dirnm + '/' + ckpntlist[0])
+    from VGGFace2.test_antonio.dataset_tools import load_for_pred
+    from VGGFace2.test_antonio.dataset_tools import evaluate_performance
+
+    """ data, Y_true = load_for_pred(val_dataset, batch_size, shape)
+    print(Y_true.shape)
+    Y_pred = res_model_multitask.predict(data, batch_size, verbose=1) """
+    Y_pred, Y_true = evaluate_performance(val_dataset, batch_size, shape, res_model_multitask)
+    print(Y_pred.shape)
+    print(Y_true.shape)
+    y_pred = Y_pred  # np.argmax(Y_pred, axis=1)
+    y_true = np.argmax(Y_true, axis=1)
+    print(y_pred.shape)
+    print(y_true.shape)
+    print(y_pred)
+    print('Confusion Matrix')
+    # predneg predpos
+    #  [[4178  659]  < True negative
+    #   [ 975 3861]] < True positive
+    from sklearn.metrics import classification_report, confusion_matrix
+
+    # y_pred = [1]*y_true.shape[0] # To try, you should give recall=1
+    conf = confusion_matrix(y_true, y_pred, [0, 1, 2, 3])
+    print(conf)
+    conf_pc = [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]]
+    mat_sum = 0
+    for row in range(NUM_CLASSES):
+        for col in range(NUM_CLASSES):
+            conf_pc[row][col] = conf[row][col] / sum(conf[row])
+            mat_sum += conf_pc[row][col]
+    print(conf_pc)
+    # we can probably use smthg like that:
+    # conf_pc2 = confusion_matrix(y_true, y_pred, [0, 1, 2, 3], normalize=True)
+
+    acc = (conf[0][0] + conf[1][1] + conf[2][2] + conf[3][3]) / mat_sum
+    print(acc)
+
+
--- VGGFace2/test_antonio/dataset_tools.py
+++ VGGFace2/test_antonio/dataset_tools.py
@@ -38,7 +38,7 @@ def load_dataset(dirpath, shape, doShuffle=False, image_process_fcn=None):
     data[3] = glob(os.path.join(dirpath, '3', '*.jpg')) + glob(os.path.join(dirpath, '3', '*.png'))
     n = [0, 0, 0, 0]
     print("African American: %d, Asian: %d, Latin: %d, Indian: %d" % (
-    len(data[0]), len(data[1]), len(data[2]), len(data[3])))
+        len(data[0]), len(data[1]), len(data[2]), len(data[3])))
     num = len(data[0]) + len(data[1]) + len(data[2]) + len(data[3])
     print(num)
 
@@ -78,42 +78,90 @@ def load_dataset(dirpath, shape, doShuffle=False, image_process_fcn=None):
         yield (im, label)
 
 
-def onlinedataaug(immagine):
-    # Rumore gaussiano
+def onlinedataaug(image):
+    # Gaussian noise
     if random.randint(0, 1):
-        row, col, ch = immagine.shape
+        row, col, ch = image.shape
         binom = np.random.binomial(n=500, p=0.5, size=(row, col, ch))
         binom = binom - 250
-        noisy = immagine + binom
+        noisy = image + binom
         noisy = np.clip(noisy, 0, 255)
-        immagine = noisy.astype(np.uint8)
-    # Ridimensionamento 
+        image = noisy.astype(np.uint8)
+    # Resize
     if random.randint(0, 1):
-        row, col, ch = immagine.shape
+        row, col, ch = image.shape
         p = random.randint(1, 2) * 2
         row = row / p
         col = col / p
-        immagine = cv2.resize(immagine, (int(row), int(col)), 0, 0, cv2.INTER_LINEAR)
-    # brightess  alternatamente aggiungo e sottraggo illuminazione
+        image = cv2.resize(image, (int(row), int(col)), 0, 0, cv2.INTER_LINEAR)
+    # brightess : alternatively adding and subtracting lighting
     if random.randint(0, 1):
-        esposizione = random.randint(1, 2)  # se 1 sovraespongo se 2 sottoespongo
+        esposizione = random.randint(1, 2)  # se 1 overexpose se 2 underexpose
         if esposizione == 2:
-            brigh = immagine * 0.7
+            brigh = image * 0.7
         else:
-            brigh = immagine * 1.4
+            brigh = image * 1.4
         brigh = np.clip(brigh, 0, 255)
-        immagine = brigh.astype(np.uint8)
+        image = brigh.astype(np.uint8)
     # flip
     if random.randint(0, 1):
-        flip = np.fliplr(immagine)
-        immagine = flip
+        flip = np.fliplr(image)
+        image = flip
 
-    return immagine
+    return image
 
 
-def full_augmentation(immagine):
-    siz = immagine.shape[0]
-    # random crop con prob + alta su 0 (gaussiana)
+def load_dataset_for_pred(dirpath, shape, doShuffle=False, image_process_fcn=None):
+    data = [None, None, None, None]
+    data[0] = glob(os.path.join(dirpath, '0', '*.jpg')) + glob(os.path.join(dirpath, '0', '*.png'))
+    data[1] = glob(os.path.join(dirpath, '1', '*.jpg')) + glob(os.path.join(dirpath, '1', '*.png'))
+    data[2] = glob(os.path.join(dirpath, '2', '*.jpg')) + glob(os.path.join(dirpath, '2', '*.png'))
+    data[3] = glob(os.path.join(dirpath, '3', '*.jpg')) + glob(os.path.join(dirpath, '3', '*.png'))
+    n = [0, 0, 0, 0]
+    print("African American: %d, Asian: %d, Latin: %d, Indian: %d" % (
+        len(data[0]), len(data[1]), len(data[2]), len(data[3])))
+    num = len(data[0]) + len(data[1]) + len(data[2]) + len(data[3])
+    print(num)
+
+    if doShuffle:
+        np.random.shuffle(data[0])
+        np.random.shuffle(data[1])
+        np.random.shuffle(data[2])
+        np.random.shuffle(data[3])
+    for i in range(NUM_CLASSES):
+        print(i)
+        for j in range(len(data[i])):
+            label = i
+            try:
+                path = data[label][n[label]]
+                n[label] += 1
+                # print(n)
+            except:
+                print("Except %s" % path)
+                continue
+            # print(label)
+            image = load_image(path, shape, image_process_fcn)
+            if image is None:
+                print("Image %s not found" % path)
+                continue
+
+            if False and shape[1] == 96:
+                vis_img = image
+                vis_img *= 255
+                vis_img = vis_img.astype(np.uint8)
+                # print((shape[1],shape[2],shape[3]))
+                vis_img = vis_img.reshape((shape[1], shape[2], shape[3]))
+                vis_img = cv2.cvtColor(vis_img, cv2.COLOR_RGB2BGR)
+                cv2.imshow("im %d" % label, vis_img)
+                cv2.waitKey(500)
+
+            im = np.array(image)
+            yield (im, label)
+
+
+def full_augmentation(image):
+    siz = image.shape[0]
+    # random crop with prob + stop on 0 (gaussian)
     sigma = 12
     mean = 0
     gauss = np.random.normal(mean, sigma, (4, 1))
@@ -125,10 +173,10 @@ def full_augmentation(immagine):
         elif gauss[i] < 0:
             gauss[i] = 0
 
-    immagine = immagine[gauss[0][0]:siz - gauss[1][0], gauss[2][0]:siz - gauss[3][0]]
+    image = image[gauss[0][0]:siz - gauss[1][0], gauss[2][0]:siz - gauss[3][0]]
 
-    immagine = onlinedataaug(immagine)
-    return immagine
+    image = onlinedataaug(image)
+    return image
 
 
 def load_for_training(dirpath, batchsize, shape):
@@ -182,3 +230,15 @@ def dataset_size(csv_path):
     for i in ds:
         count += 1
     return count
+
+
+def evaluate_performance(dirpath, batchsize, shape, network):
+    ds = load_dataset_for_pred(dirpath, shape)
+    Y_pred = []
+    Y_true = []
+    for i in ds:
+        lab = network.predict(i[0], batchsize, verbose=1)
+        print(lab)
+        Y_pred.append(np.argmax(lab))
+        Y_true.append(i[1])
+    return np.array(Y_pred), keras.utils.to_categorical(Y_true, num_classes=NUM_CLASSES)
